568cdd93b112e9e4054aa598cac164d768c25af8
Luciano Mendez <mendezluciano22@gmail.com>
2022-07-28 22:25:55 -0300

    .

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,10 @@
 const readyManos = document.querySelector(".readyManos")
 const container2 = document.querySelector(".container2")
 const volverButton = document.querySelector(".volverButton")
+const volverButton2 = document.querySelector(".volverButton2")
 const boxVideo = document.querySelector(".boxVideo")
+const readyManosrelax = document.querySelector(".readyManosrelax")
+const container3 = document.querySelector(".container3")
 
 
 
@@ -18,8 +21,8 @@ readyManos.addEventListener("click", function(){
     })
 
     gsap.to(".textos", {
-        scale:20,
-        yPercent: 400 ,
+        scale:10,
+        yPercent: 380 ,
        
         duration:1,
     })
@@ -68,9 +71,61 @@ volverButton.addEventListener("click", function(){
 })
 
 
+/* boton relax */
 
 
 
+
+/* cierra el background de manos y entra en el video */
+readyManosrelax.addEventListener("click", function(){
+
+    gsap.to(".text", {
+        opacity:0,
+    })
+
+    gsap.to(".textos", {
+        scale:30,
+        yPercent: 600 ,
+        duration:1,
+    })
+
+    setTimeout(() => {
+        container3.classList.add("container3_active")
+        
+
+    }, 1000);
+
+
+})
+
+
+volverButton2.addEventListener("click", function(){
+    
+    gsap.to(".container3", {
+        duration:0.5,
+        delay:0
+    })
+    gsap.to(".text", {
+        opacity:1,
+        delay:2
+    })
+    container3.classList.remove("container3_active")
+    setTimeout(() => {
+    
+        gsap.to(".textos", {
+            scale:1,
+            yPercent: 0 ,
+            
+            duration:1,
+            opacity:1
+        })
+    
+    }, 1000);
+
+
+
+
+})
 
 
 
